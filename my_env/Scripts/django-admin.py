a48027ce0c9776da74b64ea62b52e34eca6dc1b7
jonriber <49167364+jonriber@users.noreply.github.com>
2020-04-29 01:08:54 -0300

    updating venv

---
+++ my_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jonat\documents\programação\django\jonatas_personal_blog_project\my_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
