092486e73792653e03063f4e5e0cdd8a5ac47709
jonriber <49167364+jonriber@users.noreply.github.com>
2020-05-09 01:16:17 -0300

    Atualização de projeto

--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default = timezone.now())
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+    post = models.ForeignKey('blog.Post',on_delete= models.SET_NULL, related_name='comments', null=True)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
